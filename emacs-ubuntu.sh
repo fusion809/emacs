11417fe2cd2075566d3ec57284a6855d3833885b
fusion809 <brentonhorne77@gmail.com>
2016-10-15 19:28:17 +1000

    Adding emacs-ubuntu script

---
+++ emacs-ubuntu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PRG="$HOME/Programs"
+pkgver="25.1"
+
+if ! [[ -d "$PRG/emacs-$pkgver" ]]; then
+  wget -cqO- ftp://ftp.gnu.org/gnu/emacs/emacs-$pkgver.tar.xz | tar xJ
+fi
+
+cd $PRG/emacs-$pkgver
+./autogen.sh
+./configure --prefix=/usr --with-x-toolkit=gtk2 CFLAGS=-no-pie
+make
+sudo checkinstall --pkgversion="$pkgver" --pkgname="emacs-gtk2" --maintainer="Brenton Horne <brentonhorne77@gmail.com>"
+cp emacs-gtk2_$pkgver-1_amd64.deb ../Deb
