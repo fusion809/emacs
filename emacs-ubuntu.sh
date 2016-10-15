da4283253c0ecdba89b856e02a79dc1474cfdafa
fusion809 <brentonhorne77@gmail.com>
2016-10-15 19:31:43 +1000

    Updating

--- emacs-ubuntu.sh
+++ emacs-ubuntu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PRG="$HOME/Programs"
 pkgver="25.1"
+toolkit="gtk2"
 
 if ! [[ -d "$PRG/emacs-$pkgver" ]]; then
   wget -cqO- ftp://ftp.gnu.org/gnu/emacs/emacs-$pkgver.tar.xz | tar xJ
@@ -9,7 +10,7 @@ fi
 cd $PRG/emacs-$pkgver
 ./autogen.sh
 unset CC
-./configure --prefix=/usr --with-x-toolkit=gtk2 CFLAGS=-no-pie
+./configure --prefix=/usr --with-x-toolkit="$toolkit" CFLAGS=-no-pie
 make
-sudo checkinstall --pkgversion="$pkgver" --pkgname="emacs-gtk2" --maintainer="Brenton Horne <brentonhorne77@gmail.com>"
-cp emacs-gtk2_$pkgver-1_amd64.deb ../Deb
+sudo checkinstall --pkgversion="$pkgver" --pkgname="emacs-$toolkit" --maintainer="Brenton Horne <brentonhorne77@gmail.com>"
+cp emacs-$toolkit_$pkgver-1_amd64.deb ../Deb
